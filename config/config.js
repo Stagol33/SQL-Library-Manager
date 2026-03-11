f946c50274e98e0268065a72f8679fd77f1d5302
breck <breckc@knowbe4.com>
2026-03-11 12:09:56 -0500

    Set db to be local. It will reset every deploy

--- config/config.js
+++ config/config.js
@@ -1,25 +1,24 @@
 module.exports = {
-  development: {
-    dialect: "sqlite",
-    storage: "./library.db"
-  },
-  test: {
-    dialect: "sqlite",
-    storage: "./library.db"
-  },
-  production: {
-    // dialect: "sqlite",
-    // storage: "/tmp/library.db"
-    // Comment out PostgreSQL config temporarily
-    
-    dialect: "postgres",
-    url: process.env.DATABASE_URL,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-    
-  }
+	development: {
+		dialect: "sqlite",
+		storage: "./library.db",
+	},
+	test: {
+		dialect: "sqlite",
+		storage: "./library.db",
+	},
+	production: {
+		dialect: "sqlite",
+		storage: "./library.db",
+		// Comment out PostgreSQL config temporarily
+
+		// dialect: "postgres",
+		// url: process.env.DATABASE_URL,
+		// dialectOptions: {
+		//   ssl: {
+		//     require: true,
+		//     rejectUnauthorized: false
+		//   }
+		// }
+	},
 };
